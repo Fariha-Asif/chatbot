dc7263215e670a6c7b851836ddbf98afa30e7094
Fariha Asif <farihadua70@gmail.com>
2024-09-05 17:55:24 +0500

    push to github

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 import express from 'express';
 import ejs from 'ejs';
-import pkg from 'twilio';
-const {Twilio} = pkg;
+// import pkg from 'twilio';
+// const {Twilio} = pkg;
 
 let app = express();
 
 
 const accountSid = 'ACa02924c1e57b35de0a56c70700167e87';
 const authToken = 'cabe4602e62e1cec3171f36bf22f4bf0';
-const client = new Twilio(accountSid, authToken);
+// const client = new Twilio(accountSid, authToken);
 
 app.use(express.static('public'));
 app.set('view engine','ejs');
@@ -34,16 +34,16 @@ app.get('/contact', function(req,res){
     res.render('pages/contact')
 })
 
-function sendMessageToWhatsApp(userMessage) {
-    client.messages
-        .create({
-            body: 'User Message: ${userMessage}',
-            from: 'whatsapp: +18777804236',
-            to: 'whatsapp: +923433020932'
-        })
-        .then(message => console.log(`Message sent with SID: ${message.sid}`))
-        .catch(error => console.error(`Error sending message: ${error.message}`));
-}
+// function sendMessageToWhatsApp(userMessage) {
+//     client.messages
+//         .create({
+//             body: 'User Message: ${userMessage}',
+//             from: 'whatsapp: +18777804236',
+//             to: 'whatsapp: +923433020932'
+//         })
+//         .then(message => console.log(`Message sent with SID: ${message.sid}`))
+//         .catch(error => console.error(`Error sending message: ${error.message}`));
+// }
 
 app.post('/send-message', express.json(), (req, res) => {
     const { message } = req.body;
